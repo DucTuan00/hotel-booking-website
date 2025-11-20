0f233a818612c3b078499fc43172daccee628389
DucTuan00 <tuan03062003@gmail.com>
2025-11-20 17:17:55 +0700

    fix: scroll to top when navigate

--- client-vite/src/App.tsx
+++ client-vite/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import AppRoutes from '@/routes/AppRoutes';
+import ScrollToTop from '@/components/ScrollToTop';
 
 const App: React.FC = () => {
   return (
     <Router>
+      <ScrollToTop />
       <AppRoutes />
     </Router>
   );
